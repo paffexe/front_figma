a334970b523e81e093359b4996cd378157686f9e
paffexe <kamolov607@gmail.com>
2025-09-30 00:00:57 +0500

    1st commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/header/Header";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+      <h3>App</h3>
+    </div>
+  );
+};
+
+export default App;
