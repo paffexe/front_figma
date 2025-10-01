06ef696fc0c21f525249b39fa69b24f0862894f6
paffexe <kamolov607@gmail.com>
2025-10-01 16:19:47 +0500

    updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,7 @@ export const Home = memo(() => {
   return (
     <div className=" bg-[#2B2B2B] ">
       <div className="container">
+        <p>Added fetched users from api and 404 page with footer 90+ ?</p>
         <Hero />
         <Trending />
         <Creators/>
